9238eba4932eef1771c17217daaf5dab982ae729
Tyler Wellman <TangoKilo1985@gmail.com>
2022-08-25 07:55:12 -0400

    change

--- Model/user.js
+++ Model/user.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
+const {isEmail} = require('validator')
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter valid email']
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'Please enter password'],
+        minlength: [6, 'Must have at least 6 characters']
     }
 })
+userSchema.post('save', function (doc, next) {
+    console.log('new user created and saved', doc);
+    next();
+})
+
+userSchema.pre('save', function(next) {
+    console.log('user about to be created and saved', this);
+    next();
+})
+
 
 const User = mongoose.model('user', userSchema);
 
