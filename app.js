ef3e949823bf80cc7c561a839d0c6faa9336a4ff
Tyler Wellman <TangoKilo1985@gmail.com>
2022-08-29 07:34:48 -0400

    change

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,3 +22,22 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//app.get('/set-cookies', (req, res) => {
+  //res .setHeader('Set-cookie', 'newUser=true')
+
+  //cleaner method
+
+//  res.cookie('newUser', false);
+//  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+//  res.send('You got the cookie, check the Application tab in the inspect tool!')
+//});
+
+//app.get('/read-cookies', (req, res) => {
+
+//  const cookies = req.cookies;
+//  cookieParser.log(cookies);
+
+//  res.json(cookies);
+
+//});
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require('../Model/User')
+const User = require('../Model/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
@@ -16,6 +17,12 @@ const handleErrors = (err) => {
     }
     return errors
 }
+const maxAge = 3 * 24 * 60 * 60
+const createToken = (id) => {
+    return jwt.sign({ id }, 'secret message', {
+        expiresIn: maxAge
+    })
+}
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -29,6 +36,8 @@ module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
     try{
         const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
         res.status(201).json(user);
     
     }catch(err){
